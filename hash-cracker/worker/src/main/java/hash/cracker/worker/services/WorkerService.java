a3ed57e8b6a287406d08e3159a7cccb0eca45f02
Artem Borodin <a.borodin2@g.nsu.ru>
2023-03-14 12:18:11 +0300

    Use built-in async; Use steam for limiting combinations

--- hash-cracker/worker/src/main/java/hash/cracker/worker/services/WorkerService.java
+++ hash-cracker/worker/src/main/java/hash/cracker/worker/services/WorkerService.java
@@ -8,6 +8,7 @@ import org.paukov.combinatorics3.Generator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,61 +18,61 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.*;
 
 @Service
 public class WorkerService {
     private final String managerAddr = System.getenv("MANAGER_ADDR");
-    private final int threadCount = Integer.parseInt(System.getenv("THREAD_COUNT"));
     private final Duration taskTimeout = Duration.parse("PT" + System.getenv("TASK_TIMEOUT"));
 
-    private final BlockingQueue<Runnable> taskQueue = new LinkedBlockingQueue<>(50);;
-    private final ThreadPoolExecutor threadPool = new ThreadPoolExecutor(threadCount / 2, threadCount, 1, TimeUnit.SECONDS, this.taskQueue);
     private final RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
     Logger logger = LoggerFactory.getLogger("worker");
 
+    @Async
     public void ReceiveTask(CrackHashManagerRequest part) {
-        threadPool.submit(() -> {
-            List<String> alphabet = part.getAlphabet().getSymbols();
-            int partNumber = part.getPartNumber();
-            int partCount = part.getPartCount();
-            byte[] hashBytes = DatatypeConverter.parseHexBinary(part.getHash());
+        List<String> alphabet = part.getAlphabet().getSymbols();
+        int partNumber = part.getPartNumber();
+        int partCount = part.getPartCount();
+        byte[] hashBytes = DatatypeConverter.parseHexBinary(part.getHash());
 
-            MessageDigest hasher = null;
-            try {
-                hasher = MessageDigest.getInstance("MD5");
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
+        MessageDigest hasher = null;
+        try {
+            hasher = MessageDigest.getInstance("MD5");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
-            logger.info("Part " + partNumber + " started");
-            Instant startTime = Instant.now();
-            for (int i = 0; i <= part.getMaxLength(); i++) {
-                Iterator<List<String>> iter = Generator.permutation(alphabet).withRepetitions(i).iterator();
+        logger.info("Part " + partNumber + " started");
+        Instant startTime = Instant.now();
+        for (int i = 0; i <= part.getMaxLength(); i++) {
+            double objectCount = Math.pow(alphabet.size(), i);
+            long[] ranges = splitNumber((long) objectCount, partCount);
+            long skipCount = ranges[partNumber];
+            long limitCount = ranges[partNumber + 1] - ranges[partNumber];
 
-                long step = 0;
-                while(iter.hasNext()) {
-                    List<String> el = iter.next();
-                    if (step % partCount == partNumber) {
-                        String str = String.join("", el);
-                        byte[] md5 = hasher.digest(str.getBytes());
-                        if (Arrays.equals(md5, hashBytes)) {
-                            logger.info("Found hash: " + DatatypeConverter.printHexBinary(md5).toLowerCase() + "=" + str + ", part " + partNumber);
-                            sendAnswer(part.getRequestId(), str);
-                        }
+            Iterator<List<String>> iter = Generator.permutation(alphabet).withRepetitions(i)
+                .stream()
+                .skip(skipCount)
+                .limit(limitCount)
+                .iterator();
 
-                        Duration dur = Duration.between(startTime, Instant.now());
-                        if (dur.toMillis() > taskTimeout.toMillis()) {
-                            logger.warn("Part " + partNumber + " exceeded time limit (" + System.getenv("TASK_TIMEOUT") + "): exiting");
-                            return;
-                        }
-                    }
-                    step++;
+
+            while(iter.hasNext()) {
+                String str = String.join("", iter.next());
+                byte[] md5 = hasher.digest(str.getBytes());
+                if (Arrays.equals(md5, hashBytes)) {
+                    logger.info("Found hash: " + DatatypeConverter.printHexBinary(md5).toLowerCase() + "=" + str + ", part " + partNumber);
+                    sendAnswer(part.getRequestId(), str);
+                }
+
+                Duration dur = Duration.between(startTime, Instant.now());
+                if (dur.toMillis() > taskTimeout.toMillis()) {
+                    logger.warn("Part " + partNumber + " exceeded time limit of " + System.getenv("TASK_TIMEOUT") + ": exiting");
+                    return;
                 }
             }
+        }
 
-            logger.info("Part " + partNumber + " finished");
-        });
+        logger.info("Part " + partNumber + " finished");
     }
 
     private void sendAnswer(String id, String answer) {
@@ -81,5 +82,25 @@ public class WorkerService {
         response.getAnswers().getWords().add(answer);
 
         restTemplate.patchForObject(managerAddr + "/internal/api/manager/hash/crack/request", response, Void.class);
+    }
+
+    private static long[] splitNumber(long number, int numRanges) {
+        long[] ranges = new long[numRanges + 1];
+        long rangeSize = number / numRanges;
+        long remainder = number % numRanges;
+        int currentRange = 0;
+        long rangeEnd = 0;
+
+        for (int i = 0; i < numRanges + 1; i++) {
+            long rangeStart = rangeEnd;
+            rangeEnd = rangeStart + rangeSize;
+            if (currentRange < remainder) {
+                rangeEnd++;
+                currentRange++;
+            }
+            ranges[i] = rangeStart;
+        }
+
+        return ranges;
     }
 }
